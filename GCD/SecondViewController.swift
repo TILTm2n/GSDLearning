e326988b72c78fe1dbffb3be817ff3110dd9601d
Eugene <evgen29031999@bk.ru>
2022-01-19 21:20:11 +0300

    create imageView

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageURL: URL?
+    var image: UIImage?{
+        get{
+            return imageView.image
+        }
+        set{
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    func fetchImage(){
+        imageURL = URL(string: "https://ru.wikipedia.org/wiki/Apple#/media/%D0%A4%D0%B0%D0%B9%D0%BB:Aerial_view_of_Apple_Park_dllu.jpg")
+        if let url = imageURL{
+            do {
+                let imageData = try Data(contentsOf: url)
+                self.image = UIImage(data: imageData)
+                print("daflasjdfjdas")
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        fetchImage()
     }
     
 
