2ad04da015ee1a26e2e70671a04bb63f24e6b55b
Eugene <evgen29031999@bk.ru>
2022-01-22 15:26:19 +0300

    add DispatchGroup

--- GCD/DataManager.swift
+++ GCD/DataManager.swift
@@ -10,12 +10,34 @@ import Foundation
 class DataManager{
     //функции помеченные class позволяют подклассам переопределять инструкции суперкласса этого метода
     class func obtainData(completion: @escaping (([String]) -> Void)){
-        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
-            
-            //completion нужно возвращать в главном потоке
-            DispatchQueue.main.async {
-                completion(["hello","all","there","!"])
-            }
+        
+        var data = [String]()
+        
+        let group = DispatchGroup()
+        
+        group.enter()
+        DispatchQueue.global(qos: .utility).async{
+            data.append("all")
+            group.leave()
         }
+        
+        group.enter()
+        DispatchQueue.global().async{
+            data.append("hello")
+            group.leave()
+        }
+        
+        group.enter()
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            data.append("there")
+            data.append("!!!")
+            group.leave()
+        }
+        
+        //notify срабатывает когда количество входов равно количеству выходов
+        group.notify(queue: .main) {
+            completion(data)
+        }
+        
     }
 }
